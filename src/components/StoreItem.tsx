850bda642fed5d08c5dd6f2e1dd0511aa9834d18
Amos Babu <okumuamos88@gmail.com>
2024-09-13 17:01:00 +0300

    "Finished "

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,55 +1,69 @@
-import { Button, Card } from "react-bootstrap"
-import { formatCurrency } from "../utilities/formatCurrency"
+import { Button, Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 type StoreItemProps = {
-    id: number,
-    name: string,
-    price: number,
-    imgUrl: string
-}
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
 
-const StoreItem = ({ id, name, price, imgUrl}: StoreItemProps) => {
-    const quantity = 0
+const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
   return (
     <Card className="h-100">
-        <Card.Img
-            variant="top"
-            src={imgUrl}
-            height="200px"
-            style={{objectFit: "cover"}}
-        />
-        <Card.Body className="d-flex flex-column">
-            <Card.Title 
-                className="d-flex justify-content-between align-items-baseline mb-4">
-                    <span className="fs-2">{name}</span>
-                    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
-            </Card.Title>
-            <div className="mt-auto">
-                {quantity === 0 ? (
-                    <Button
-                        className="w-100"
-                    >+ Add To Cart</Button>
-                ):(
-                    <div 
-                        className="d-flex align-items-center flex-column "
-                        style={{gap: ".5rem"}}>
-
-                            <div 
-                                className="d-flex align-items-center justify-content-center "
-                                style={{gap: ".5rem"}}>
-                                    <Button>-</Button>
-                                        <div>
-                                            <span className="fs-3">{quantity}</span> in cart
-                                        </div>
-                                    <Button>+</Button>
-                            </div>
-                            <Button className="bg-danger" size="sm">Remove</Button>
-                    </div>
-                )}
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+              + Add To Cart
+            </Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column "
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center "
+                style={{ gap: ".5rem" }}
+              >
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+              </div>
+              <Button
+                onClick={() => removeFromCart(id)}
+                className="bg-danger"
+                size="sm"
+              >
+                Remove
+              </Button>
             </div>
-        </Card.Body>
+          )}
+        </div>
+      </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default StoreItem
+export default StoreItem;
